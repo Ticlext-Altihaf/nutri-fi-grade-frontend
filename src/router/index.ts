aa80776e6eeac3b12b7b3e693af7b7283371b3ca
Ticlext-Altihaf <yusufsekhanalthaf@gmail.com>
2024-11-02 10:28:59 +0700

    🗑️ refactor(VueBarcodeScanner): remove obsolete barcode scanner components and integrate Quagga scanner view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import VueBarcodeCameraView from '../views/VueBarcodeCameraView.vue'
+import QuaggaScannerView from '../views/QuaggaScannerView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'camera',
-      component: VueBarcodeCameraView,
+      component: QuaggaScannerView,
     },
   ],
 })
